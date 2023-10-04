0d850c61db7dc9026ee7632e9caaffebfe019ad5
Mohammad Sayful Islam <Sayf.mohammad01@gmail.com>
2023-10-04 08:05:44 +0100

    murray: use OSS fingerprint hal

--- device.mk
+++ device.mk
@@ -310,7 +310,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1.vendor
+    android.hardware.biometrics.fingerprint@2.1-service.sony
 
 PRODUCT_PACKAGES += \
     android.hardware.gnss.measurement_corrections@1.1.vendor \
